9158a5a050eee20d864558a573d8bf0abb2534fa
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-07-06 15:57:16 +0530

    button navigation

--- src/Buttonnav.jsx
+++ src/Buttonnav.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 function Buttonnav(){
+    const navigate = useNavigate()
     return(
         <div>
-
+            <button onClick={()=>{
+                navigate("/media")
+            }}>Media</button>
         </div>
     );
 }

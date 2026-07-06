fafaf3847646e7cd2d179151018b6a8cb6a121b3
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-07-06 11:42:11 +0530

    video tag

--- src/Media.jsx
+++ src/Media.jsx
@@ -4,6 +4,10 @@ function Media(){
         <div>
             <h1>Media</h1>
             <img src={img1}/>
+
+            <video controls>
+                <source src="https://res.cloudinary.com/dir0f6ufp/video/upload/v1767470416/media_user/1/post_1_1767470403795_raviteja.mp4.mp4" type="video/mp4"/>
+            </video>
         </div>
     )
 }

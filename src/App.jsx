fc620c4291df7a69c2752a399797cda87e11cf5e
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-07-06 14:46:25 +0530

    path for button nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Media from "./Media";
 import {Routes,Route} from 'react-router-dom'
 import Navtag from "./Navtag"
 import Linktag from "./Linktag"
+import Buttonnav from "./Buttonnav";
 function App(){
   return(
    <>
     <Routes>
       <Route path="/media" element={<Media/>} />
       <Route path="/counter" element={<Counter/>} />
-      <Route path="/" element={<Navtag/>}/>
+      <Route path="/navtag" element={<Navtag/>}/>
       <Route path="/linktag" element={<Linktag/>}/>
+      <Route path="/" element={<Buttonnav/>}/>
     </Routes>
    </>
   )
---
+++ src/Buttonnav.jsx
@@ -0,0 +1,9 @@
+function Buttonnav(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Buttonnav;
